c1ec3dfd171b242e23b3fe078a99f0e23fb0c6ea
Stefan Kögl <stefan@skoegl.net>
2013-04-03 15:42:02 +0200

    bump version to 1.0

--- jsonpointer.py
+++ jsonpointer.py
@@ -34,7 +34,7 @@
 
 # Will be parsed by setup.py to determine package metadata
 __author__ = 'Stefan Kögl <stefan@skoegl.net>'
-__version__ = '0.7'
+__version__ = '1.0'
 __website__ = 'https://github.com/stefankoegl/python-json-pointer'
 __license__ = 'Modified BSD License'
 
